6a5c382cab31a78886ebe2396925c655f8b5e194
BOULET Pierre <pierre.boulet@maif.fr>
2026-07-02 11:00:08 +0200

    Initial commit with clean files

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Form
+
+Small project from school in order to learn how to manage forms with React.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <p>Mon formulaire React</p>
+      </section>
+    </>
+  )
+}
+
+export default App
